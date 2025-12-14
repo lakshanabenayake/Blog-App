49eb42582f6b904bb8d615e931ab6fa78bc81633
lakshanabenayake <lakshanabenayake@gmail.com>
2025-12-14 20:37:08 +0530

    add GetUserById method to IauthService interface

--- backend/Interfaces/IauthService.cs
+++ backend/Interfaces/IauthService.cs
@@ -6,4 +6,5 @@ public interface IauthService
     Task<AuthResponseDTO?> LoginAsync(LoginDTO loginDto);
     Task<AuthResponseDTO?> RegisterAsync(RegisterDTO registerDto);
     Task<UserDTO?> UpdateProfileAsync(Guid userId, UpdateProfileDTO dto);
+    Task<UserDTO?> GetUserById(Guid userId);
 }
